452d577a34f542298ea90b7f620c92f7fd0169ba
samthesoup <sambartgiss@gmail.com>
2026-02-05 13:06:34 -0500

    improved player physics

--- player.gd
+++ player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("in_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -19,10 +19,14 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if !is_on_floor():
+			velocity.x = move_toward(velocity.x, direction.x*SPEED, SPEED*0.1)
+			velocity.z = move_toward(velocity.z, direction.z*SPEED, SPEED*0.1)
+		else: 
+			velocity.x = direction.x*SPEED
+			velocity.z = direction.z*SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x 		= move_toward(velocity.x, 0, SPEED*0.1)
+		velocity.z = move_toward(velocity.z, 0, SPEED*0.1)
 
 	move_and_slide()
