35ccff633bf49f009208e74e289dc49ef38800c4
samthesoup <sambartgiss@gmail.com>
2026-02-15 11:03:05 -0500

    created enemy class

---
+++ enemy.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+class_name Enemy
+
+@export var health : int
+@export var damage : int
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
