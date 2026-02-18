82abe2983a2d31f5fecaeeb7901ceaa561a2780a
samthesoup <sambartgiss@gmail.com>
2026-02-18 13:42:16 -0500

    export settings

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var player: CharacterBody3D = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if get_tree().get_nodes_in_group("Enemies").size() <= 0:
+		player.can_progress = true
