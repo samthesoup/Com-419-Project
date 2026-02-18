040e547f6af92c61ed5bc3a1440ef74a4fc8c072
samthesoup <sambartgiss@gmail.com>
2026-02-18 13:21:49 -0500

    enemy approach distance

--- small_enemy.gd
+++ small_enemy.gd
@@ -3,17 +3,21 @@ extends Enemy
 @export var target : Node
 @onready var floor_ray: RayCast3D = $"Floor Ray"
 
+var approach_dist = 0.82
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var target_dir = position.direction_to(target.position)
 	var mo : Vector3 = Vector3(target_dir.x,0,target_dir.z)*delta
+	if position.distance_to(target.position) <= approach_dist:
+		mo = Vector3(0,0,0)
 	if !floor_ray.is_colliding():
-		mo.y = -1*delta
+		mo.y = -3*delta
 	move_and_collide(mo)
 
 
